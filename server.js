c3de3cb833d7fd41d63488cb12ee74fe7445a76f
Marvin Meng <marvinmeng11@gmail.com>
2021-01-03 00:58:02 -0500

    Add GET all quotes request

--- server.js
+++ server.js
@@ -12,5 +12,22 @@ app.get('/api/quotes/random', (req, res, next) => {
 	res.send(getRandomElement(quotes));
 });
 
+app.get('/api/quotes', (req, res, next) => {
+	if (req.query) {
+		const quoteArray = [];
+		const author = req.query.author;
+		for (let i = 0; i < quotes.length; i++) {
+			if (quotes[i]["person"] === author) {
+				quoteArray.push(quotes[i]["quote"]);
+			}
+		}
+		res.send(quoteArray);
+	} else {
+		res.send(quotes);
+	}
+});
+
+
+
 
 app.listen(PORT);
